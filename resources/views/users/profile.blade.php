dea748728ccc6e92e35e104b5c23582c130d589d
Islam Youssief <Islam.youssief@gmail.com>
2019-04-29 01:33:10 +0200

    showing specific user info

---
+++ resources/views/users/profile.blade.php
@@ -0,0 +1,29 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/homePage.css') }}" >    
+</head>
+<body class="profile-page">
+    <div class="page-header header-filter" data-parallax="true" style="background-image:url('{{ asset('images/userbg.jpg') }}');"></div>
+    <div class="main main-raised">
+        <div class="profile-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 ml-auto mr-auto">
+                       <div class="profile">
+                            <div class="avatar">
+                                <img src='{{ asset("images/user.jpg") }}' alt="Circle Image" class="img-raised rounded-circle img-fluid">
+                            </div>
+                            <div class="name">
+                                <h1 class="title">{{$users->user_name}}</h1> 
+                                <h4 class="userinfo"> National ID: {{$users->national_id}}</h4>
+                                <h4 class="userinfo">Phone: {{$users->phone}}</h4>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+</body>
+
