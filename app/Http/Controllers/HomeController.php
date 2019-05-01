c11842faa4d201977c64e111ce4c363f48c16414
Islam Youssief <Islam.youssief@gmail.com>
2019-05-01 07:55:58 +0200

    Implementing home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
+use App\Category;
+use App\Book_Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -17,12 +21,16 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show the application home page.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        return view('home');
+     
+        $books = Book::all();
+        $categories = Category::all();
+        return view('home', compact('books','categories'));
+
     }
 }
