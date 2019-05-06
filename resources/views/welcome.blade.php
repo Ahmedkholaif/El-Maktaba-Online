dedb4a61396a4380cf4e45d98e739e096d8386ae
A.Kholaif <ahmed_kholaif@yahoo.com>
2019-05-06 14:09:04 +0200

    push to production && debug mode=false

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,9 +32,7 @@
 {{-- @yield('login') --}}
         {{-- <div class="flex-center position-ref full-height"> --}}
         
-                <a class="navbar-brand" href="{{ url('/users') }}">
-                    Users
-                 </a>
+            
         @include('auth.login')
         
             {{-- @if (Route::has('login'))
